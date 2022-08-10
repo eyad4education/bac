1601d91858c6b4d75fd824d190f0fb26b0fc4d04
Ayodo <37110623+ayoodo@users.noreply.github.com>
2022-08-10 11:18:52 +0100

    pascal

---
+++ Personal/pascal/pascal_v2.py
@@ -0,0 +1,15 @@
+n = 5
+M = [([int()] * n) for i in range(n)]
+
+for i in range(n):
+    for j in range(n):
+        if i == j or j == 0:
+            M[i][j] = 1
+        else:
+            if i > j:
+                M[i][j] = M[i-1][j] + M[i-1][j-1]
+
+for j in range(n):
+    for i in range(n):
+        print(M[j][i], end=" ")
+    print()
