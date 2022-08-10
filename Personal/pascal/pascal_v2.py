44b3e1c4c2b1dcb49872d1ce17b1cf68540b0c55
Ayodo <37110623+ayoodo@users.noreply.github.com>
2022-08-10 11:23:28 +0100

    pascal

--- Personal/pascal/pascal_v2.py
+++ Personal/pascal/pascal_v2.py
@@ -1,12 +1,12 @@
 n = 5
 M = [([int()] * n) for i in range(n)]
 
-for i in range(n):
-    for j in range(n):
+for j in range(n):
+    for i in range(n):
         if i == j or j == 0:
-            M[i][j] = 1
-        elif i > j:
-            M[i][j] = M[i-1][j] + M[i-1][j-1]
+            M[j][i] = 1
+        elif j > i:
+            M[j][i] = M[j-1][i] + M[j-1][i-1]
 
 for j in range(n):
     for i in range(n):
