e4bda854172bfdee6d763bc15090cc5a17c6a868
Ayodo <37110623+ayoodo@users.noreply.github.com>
2022-08-10 11:21:52 +0100

    pascal

--- Personal/pascal/pascal_v2.py
+++ Personal/pascal/pascal_v2.py
@@ -5,9 +5,8 @@ for i in range(n):
     for j in range(n):
         if i == j or j == 0:
             M[i][j] = 1
-        else:
-            if i > j:
-                M[i][j] = M[i-1][j] + M[i-1][j-1]
+        elif i > j:
+            M[i][j] = M[i-1][j] + M[i-1][j-1]
 
 for j in range(n):
     for i in range(n):
