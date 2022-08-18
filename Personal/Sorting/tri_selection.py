2c92dc9f31c876eb86735ee5a8ce5b9d8498c729
Ayodo <37110623+ayoodo@users.noreply.github.com>
2022-08-18 18:10:38 +0100

    tri

---
+++ Personal/Sorting/tri_selection.py
@@ -0,0 +1,14 @@
+n = 5
+t = [3,1,2,5,4]
+
+for i in range(n):
+    index = i
+    for j in range(i+1, n):
+        if t[j] < t[index]:
+            index = j
+    if i != index:
+        t[i] = t[i] + t[index]
+        t[index] = t[i] - t[index]
+        t[i] = t[i] - t[index]
+
+print(t)
