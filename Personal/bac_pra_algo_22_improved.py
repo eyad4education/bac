7b498816add48d428074f8b0f6a46ac07d1e7209
Ayodo <37110623+ayoodo@users.noreply.github.com>
2022-08-23 18:05:50 +0100

    bac_pra_algo_22_improved.py

---
+++ Personal/bac_pra_algo_22_improved.py
@@ -0,0 +1,86 @@
+from math import sqrt
+
+
+def estPremier(n):
+    flag = True
+    i = 2
+    while i <= sqrt(n) and flag:
+        if n % i == 0:
+            flag = False
+        i += 1
+    return flag
+
+
+def dec_to_bin(n):
+    ch = ""
+    while n != 0:
+        ch = str(n % 2) + ch
+        n = n // 2
+    return ch
+
+
+def valider(code):
+    bin = dec_to_bin(int(code[3:8]))
+    nbz = 0
+    for i in range(len(bin)):
+        if bin[i] == "0":
+            nbz += 1
+    if estPremier(int(code[0:3])) and nbz > 8 and (int(code[8:]) % int(code[0:3])) == 0:
+        flag = True
+    else:
+        flag = False
+    return flag
+
+
+def saisir():
+    global T
+    v = False
+    while not v:
+        code = str(input("donner code: "))
+        v = code.isnumeric() and len(code) == 13
+
+    if not valider(code):
+        print("Code non valide")
+    elif code not in T:
+        print("Code deja utilise")
+    else:
+        print("Code Valide")
+        # REMOVING THE USED CODE.
+        remove_used_code(fch, T, long, code)
+
+
+def num_lignes(fch):
+    F = open(fch, "r")
+    long = 0
+    ch = F.readline()
+    while ch != "":
+        long = long + 1
+        ch = F.readline()
+    F.close()
+    return long
+
+
+def file_tab(fch, long):
+    global T
+    F = open(fch, "r")
+    for i in range(long):
+        T[i] = F.readline().rstrip()
+    F.close()
+
+
+# ADDITION... SHOULDN'T THE CODE USED BE REMOVED FROM THE "Codes.txt" file?
+
+
+def remove_used_code(fch, T, long, code):
+    F = open(fch, "w")
+    for i in range(long):
+        if not (T[i] == code):
+            F.write(T[i] + "\n")
+    F.close()
+
+
+fch = "Codes.txt"
+long = num_lignes(fch)
+T = [str()] * long
+file_tab(fch, long)
+saisir()
